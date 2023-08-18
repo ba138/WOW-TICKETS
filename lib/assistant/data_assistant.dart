824ab63f68a8ffef9d916616c147d5519bd59eec
ba138 <basitalyshah51214@gmail.com>
2023-08-19 00:08:21 +0500

    adding error handling

--- lib/assistant/data_assistant.dart
+++ lib/assistant/data_assistant.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -6,22 +7,28 @@ import 'package:http/http.dart' as http;
 
 class DataAssistant {
   Future<void> fetchDataAndStore(String sellerID) async {
-    final url = Uri.parse(
-        'https://wow-tickets-app-staging.up.railway.app/api/orders/sales?seller_id=$sellerID');
-    final response = await http.get(
-      url,
-    );
-    if (response.statusCode == 200) {
-      final jsonData = json.decode(response.body);
-
-      final databasesPath = await getDatabasesPath();
-      final path = join(
-        databasesPath,
-        'WOWOTICKETSSCANN',
+    try {
+      final url = Uri.parse(
+          'https://wow-tickets-app-staging.up.railway.app/api/orders/sales?seller_id=$sellerID');
+      final response = await http.get(
+        url,
       );
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
 
-      final db = await openDatabase(path);
-      await db.insert('Events', jsonData);
+        final databasesPath = await getDatabasesPath();
+        final path = join(
+          databasesPath,
+          'WOWOTICKETSSCANN',
+        );
+
+        final db = await openDatabase(path);
+        await db.insert('Events', jsonData);
+      }
+    } catch (e) {
+      debugPrint(
+        e.toString(),
+      );
     }
   }
 }
