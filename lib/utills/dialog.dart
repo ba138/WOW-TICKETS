472736c0eb198ec2eeff901f80e9bfa45d6e8312
ba138 <basitalyshah51214@gmail.com>
2023-08-21 01:30:55 +0500

    changes to add dialog box

---
+++ lib/utills/dialog.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:wowtickets/constants.dart';
+
+class Alert extends StatelessWidget {
+  const Alert({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: backgroundColor,
+      child: Container(),
+    );
+  }
+}
