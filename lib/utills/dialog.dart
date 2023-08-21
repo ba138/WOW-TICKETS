ef1f7e30ffaadbdc0b7b1e5cd6753b5c20e4f0f2
ba138 <basitalyshah51214@gmail.com>
2023-08-21 15:04:08 +0500

    changes in ui

--- lib/utills/dialog.dart
+++ lib/utills/dialog.dart
@@ -6,7 +6,6 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:wowtickets/constants.dart';
 
 import '../Database_Helper/database_update.dart';
-import '../auth/auth_provider.dart';
 
 class Alert extends StatefulWidget {
   const Alert({super.key});
@@ -18,46 +17,76 @@ class Alert extends StatefulWidget {
 class _AlertState extends State<Alert> {
   @override
   Widget build(BuildContext context) {
-    AuthProvider authProvider = AuthProvider();
-
     return Dialog(
       backgroundColor: backgroundColor,
-      child: Card(
-        child: Column(
-          children: [
-            const Text("Are you want to sync data"),
-            Row(
+      child: Container(
+        height: 150,
+        width: double.infinity,
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Column(
               children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text("No"),
+                const Text(
+                  "Are you want to sync data",
+                  style: TextStyle(fontSize: 18, fontFamily: "Poppins"),
                 ),
-                ElevatedButton(
-                  onPressed: () async {
-                    var connectivityResult =
-                        await (Connectivity().checkConnectivity());
-                    if (connectivityResult == ConnectivityResult.mobile ||
-                        connectivityResult == ConnectivityResult.wifi) {
-                      final dbUpdater = DatabaseUpdater();
-                      await dbUpdater.initDatabase();
-                      dbUpdater.performPatchAction(
-                        context,
-                      );
-                      Navigator.pop(context);
-                      // Connected to mobile data or Wi-Fi
-                    } else {
-                      Fluttertoast.showToast(
-                        msg: "Please check your internet connection",
-                        backgroundColor: wrongColor,
-                        textColor: backgroundColor,
-                      ); // Not connected
-                    }
-                  },
-                  child: const Text("Yes"),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 100,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: primaryColor,
+                          ),
+                          child: const Text("No"),
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            var connectivityResult =
+                                await (Connectivity().checkConnectivity());
+                            if (connectivityResult ==
+                                    ConnectivityResult.mobile ||
+                                connectivityResult == ConnectivityResult.wifi) {
+                              final dbUpdater = DatabaseUpdater();
+                              await dbUpdater.initDatabase();
+                              dbUpdater.performPatchAction(
+                                context,
+                              );
+                              Navigator.pop(context);
+                              // Connected to mobile data or Wi-Fi
+                            } else {
+                              Fluttertoast.showToast(
+                                msg: "Please check your internet connection",
+                                backgroundColor: wrongColor,
+                                textColor: backgroundColor,
+                              ); // Not connected
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: primaryColor,
+                          ),
+                          child: const Text("Yes"),
+                        ),
+                      )
+                    ],
+                  ),
                 )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
