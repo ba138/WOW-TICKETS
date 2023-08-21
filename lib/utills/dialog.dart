13af4b1c86d5317a961a25b007aa6178b4ddbfc5
ba138 <basitalyshah51214@gmail.com>
2023-08-21 14:22:38 +0500

    checking connectivity

--- lib/utills/dialog.dart
+++ lib/utills/dialog.dart
@@ -1,11 +1,25 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:wowtickets/constants.dart';
 
-class Alert extends StatelessWidget {
+import '../Database_Helper/database_update.dart';
+import '../auth/auth_provider.dart';
+
+class Alert extends StatefulWidget {
   const Alert({super.key});
 
   @override
+  State<Alert> createState() => _AlertState();
+}
+
+class _AlertState extends State<Alert> {
+  @override
   Widget build(BuildContext context) {
+    AuthProvider authProvider = AuthProvider();
+
     return Dialog(
       backgroundColor: backgroundColor,
       child: Card(
@@ -19,7 +33,26 @@ class Alert extends StatelessWidget {
                   child: const Text("No"),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    var connectivityResult =
+                        await (Connectivity().checkConnectivity());
+                    if (connectivityResult == ConnectivityResult.mobile ||
+                        connectivityResult == ConnectivityResult.wifi) {
+                      final dbUpdater = DatabaseUpdater();
+                      await dbUpdater.initDatabase();
+                      dbUpdater.performPatchAction(
+                        context,
+                      );
+                      Navigator.pop(context);
+                      // Connected to mobile data or Wi-Fi
+                    } else {
+                      Fluttertoast.showToast(
+                        msg: "Please check your internet connection",
+                        backgroundColor: wrongColor,
+                        textColor: backgroundColor,
+                      ); // Not connected
+                    }
+                  },
                   child: const Text("Yes"),
                 )
               ],
